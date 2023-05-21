b4a6493d384f076748c28140661caabc47462e38
João Pedro <joao2015.76@gmail.com>
2023-05-20 21:38:47 -0300

    Adicionado método POST no formulário de registro de visitante

--- visitantes/views.py
+++ visitantes/views.py
@@ -9,13 +9,19 @@ def registrar_visitante(request):
         form = VisitanteForm(request.POST)
 
         if form.is_valid():
+            # Salva os dados do formulário no banco de dados, mas não faz o commit
             visitante = form.save(commit=False)
 
+            # Define o campo "registrado_por" do visitante como o porteiro atualmente autenticado
             visitante.registrado_por = request.user.porteiro
+
+            # Salva o visitante no banco de dados
             visitante.save()
 
             return redirect("index")
 
+    # Cria um contexto contendo o nome da página e o formulário
+    # Com o contexto se consegue utilizar o valor de cada chave diretamente no HTML
     context = {
         "nome_pagina" : "Registrar Visitante",
         "form" : form,
