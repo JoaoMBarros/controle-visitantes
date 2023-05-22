35b5a702b0fbe2f362321c6b59c88aeca0c80ed2
João Pedro <joao2015.76@gmail.com>
2023-05-22 18:52:07 -0300

    Adicionado botão para finalizar visita

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 from usuarios.views import index
-from visitantes.views import registrar_visitante, informacoes_visitante
+from visitantes.views import registrar_visitante, informacoes_visitante, finalizar_visita
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,5 +23,11 @@ urlpatterns = [
         "visitantes/<int:id>/",
         informacoes_visitante,
         name="informacoes_visitante"
+    ),
+
+    path(
+        "visitantes/<int:id>/finalizar_visita",
+        finalizar_visita,
+        name="finalizar_visita"
     )
 ]
